e936ed89b01aa06b1986c6eb13a5a33f52b603f3
ModuleCode <modulecode@126.com>
2023-01-12 01:51:21 +0800

    添加了资源文件夹

--- src/main/java/com/modulecode/Application.java
+++ src/main/java/com/modulecode/Application.java
@@ -8,12 +8,7 @@ public class Application {
     private static Logger logger = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
-        //1 Create the server object
-        var server = new Server();
-        //2 Configure user-defined routes and services
-        server.addRouter(0, new PingRouter());
-        //3 Start the service
-        server.serve();
+
 
 //        logger.trace("trace level");
 //        logger.debug("debug level");

de337c0f3ca753ec5bb062bdbc264ff48c5bf423
ModuleCode <modulecode@126.com>
2023-01-12 01:49:29 +0800

    改变了启动类的名字

--- src/main/java/com/modulecode/Application.java
+++ src/main/java/com/modulecode/Application.java
@@ -1,12 +1,20 @@
 package com.modulecode;
 
+import com.modulecode.net.impl.Server;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Main {
-    private static Logger logger = LogManager.getLogger(Main.class);
+public class Application {
+    private static Logger logger = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
+        //1 Create the server object
+        var server = new Server();
+        //2 Configure user-defined routes and services
+        server.addRouter(0, new PingRouter());
+        //3 Start the service
+        server.serve();
+
 //        logger.trace("trace level");
 //        logger.debug("debug level");
 //        logger.info("info level");
